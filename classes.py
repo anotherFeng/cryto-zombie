78605a56f178526e7b868827c72aa5776c705db7
anotherFeng <regnagleppod@gmail.com>
2018-11-25 16:29:25 -0500

    Add class

--- classes.py
+++ classes.py
@@ -1,2 +1,39 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+class User:
+  def __init__(self, name, email, age):
+    self.name = name
+    self.email = email
+    self.age = age
+
+  def greeting(self):
+    return f'My name is {self.name} and I am {self.age}'
+
+  def has_birthday(self):
+    self.age += 1
+
+class Customer(User):
+  def __init__(self, name, email, age):
+    self.name = name
+    self.email = email
+    self.age = age
+    self.balance = 0
+
+  def set_balance(self, balance):
+    self.balance = balance
+    # return super().__init__(name, email, age)
+  
+  def greeting(self):
+    return f'My name is {self.name} and I am {self.age} and I owe a balance of {self.balance}'
+
+brad = User('Brad', 'brad@gmail.com', 37)
+
+brad.has_birthday()
+
+print(brad.age)
+print(brad.greeting())
+
+john = Customer('John', 'john@gmail.com', 40)
+john.set_balance(500)
+
+print(john.greeting())
--- modules.py
+++ modules.py
@@ -1,1 +1,18 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+import datetime
+today = datetime.date.today()
+
+# from datetime import date
+# today = date.today()
+
+from time import time
+timestamp = time()
+
+print(today)
+print(timestamp)
+
+import camelcase
+camel = camelcase.CamelCase()
+text = 'hello there world'
+print(camel.hump(text))
