165899a231ee5614f108ff27eab57941ef3ad844
anotherFeng <regnagleppod@gmail.com>
2018-11-25 16:35:41 -0500

    Add files

--- files.py
+++ files.py
@@ -1,1 +1,18 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+myFile = open('myFile.txt', 'w')
+
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+myFile.write('I love Python')
+myFile.close()
+
+myFile = open('myFile.txt', 'a')
+myFile.write(' and Javascript')
+myFile.close()
+
+myFile = open('myFile.txt', 'r+')
+text = myFile.read(10)
+print(text)
